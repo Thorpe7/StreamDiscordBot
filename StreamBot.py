40fa540df9596dd322d67cfcc8250169dd64fd09
Thorpe7 <Maxwell.Thorpe7@gmail.com>
2021-10-17 14:33:58 -0500

    Commit of interaction w/ twitch api and discord bot

--- StreamBot.py
+++ StreamBot.py
@@ -1,6 +1,53 @@
+from asyncio.exceptions import LimitOverrunError
+from datetime import timedelta
+from time import sleep
 import discord
+import os
+import datetime
+from discord import message
 
+from discord.message import flatten_handlers
+from StreamerStatus import Streamer
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+from discord.ext.commands import has_permissions
+
+
+# load env vars and streamer config
+load_dotenv()
+time = datetime.datetime.now
+cyd = Streamer("config.json")
+cyd.getAccessToken()
 
 
 # Create instance of client
-client = discord.Client()
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+async def announce():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(807057710222147635)
+    alreadyLive = 'no'
+    while True:
+        status = cyd.isLive()
+        if status == "live" and alreadyLive =='no':
+            await channel.send("Cyd is now LIVE on Twitch :partying_face:\n https://www.twitch.tv/originalcyd")
+            alreadyLive = 'yes'
+        if status != 'live' and alreadyLive == 'yes':
+            alreadyLive = 'no'
+        else: 
+            pass
+
+        await asyncio.sleep(1800)
+        
+            
+
+
+bot.loop.create_task(announce())
+streamBotToken = os.environ.get('STREAMBOT_TOKEN')
+bot.run(streamBotToken)
--- StreamerStatus.py
+++ StreamerStatus.py
@@ -1,24 +1,47 @@
 import requests
 import json
 import sys
-from dotenv import load_dotenv
+import os
+from requests import api
+from requests.api import post, request
+from requests.models import Response
 
 # Create Streamer Class:
 class Streamer:
-    '''Streamer class with url instance'''
-    def __init__(self, url):
-        self.url = url
+    '''Streamer class with url instance, config json file must be in bots directory'''
+    def __init__(self,configParamsJson):
+        self.configPath = os.path.join(os.getcwd(),configParamsJson)
+        with open(self.configPath) as config_file:
+            config = json.load(config_file)
+        self.config = config
+        self.url = self.config["url"]
+        config_file.close()
+
+    def getAccessToken(self):
+        '''Retrieves twitch access token and stores in config file,
+        token good for 60 days'''
+        tokenUrl = "https://id.twitch.tv/oauth2/token"
+        response = requests.post(tokenUrl,self.config)
+        accessToken = response.json()['access_token']
+        self.config.update({"access_token":accessToken})
+        with open("config.json", "w") as outfile:
+            json.dump(self.config, outfile)
+        outfile.close()
 
     def isLive(self):
         '''Will return string indicating if the streamer is live or not'''
-        channel = requests.get(self.url, headers=headers)
-        channelData = channel.json()
-        return channelData['data']
+        with open("config.json", "rb") as config_file:
+            self.config = json.load(config_file)
+        config_file.close()
+        params = {
+            "login":self.config["user_login"]
+        }
+        headers = {
+            "Authorization": "Bearer {}".format(self.config['access_token']),
+            'Client-Id': self.config['client_id']
+            }
+        response = requests.get(self.url,params=params, headers=headers).json()['data']
+        status = response[0]['type']
+        return status # returns empty string if not live, otherwise returns 'live'
+        
 
-twitch1 = Streamer("https://www.twitch.tv/originalcyd")
-print(twitch1.isLive())
-
-# Set up app on heroku
-# Set up storing of information
-# get authentification from twitch
-# pull streamer data
